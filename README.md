8c1ccfd74826574e9ffe0032376dabd5bc0a9bab
ryan <v-rmiao@expedia.com>
2017-11-15 23:16:21 +0800

    readme

---
+++ README.md
@@ -0,0 +1,3 @@
+SpringBoot Demo
+====
+
--- src/main/java/com/test/demo/controller/HelloController.java
+++ src/main/java/com/test/demo/controller/HelloController.java
@@ -3,9 +3,6 @@ package com.test.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by Ryan on 2017/11/14/0014.
@@ -13,10 +10,6 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 public class HelloController {
 
-    @GetMapping("/")
-    public String index(){
-        return hello();
-    }
 
     @ResponseBody
     @GetMapping("/hello")
