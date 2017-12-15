8fdb0ecaa1becc15aa794cc795be9e5999992f40
ryan <v-rmiao@expedia.com>
2017-12-15 16:28:52 +0800

    增加复杂查询JPA支持

--- src/main/java/com/test/demo/domain/dao/IRoomRepository.java
+++ src/main/java/com/test/demo/domain/dao/IRoomRepository.java
@@ -2,7 +2,8 @@ package com.test.demo.domain.dao;
 
 import com.test.demo.domain.entity.RoomTable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface IRoomRepository extends JpaRepository<RoomTable, Integer> {
+public interface IRoomRepository extends JpaRepository<RoomTable, Integer>, JpaSpecificationExecutor<RoomTable> {
 
 }
